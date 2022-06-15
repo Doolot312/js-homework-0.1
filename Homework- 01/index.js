68c10ef4c16a00c80b28a4f85f73a74add45ad81
Doolot312 <106803960+Doolot312@users.noreply.github.com>
2022-06-15 22:06:28 +0600

    Add files via upload

---
+++ Homework- 01/index.js
@@ -0,0 +1,55 @@
+// Задача #1
+const userName = 'John';
+const greeting1 = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDessert = 'cake';
+let _pet = 'dog';
+let superNumber;
+superNumber = 17;
+// Задача #2
+const a = 17;
+const b = 17.17;
+const c = '17';
+const d = "17";
+const e = "I'm a string";
+const f = false;
+const g = true;
+const h = null;
+const i = undefined;
+const j = Symbol("I'm a Symbol");
+const k = { name: 'John' };
+const l = [1, 2, 3, 4];
+const m = function run() {
+    return 17;
+};
+console.log("a: ", typeof a);
+console.log("b: ", typeof b);
+console.log("c: ", typeof c);
+console.log("d: ", typeof d);
+console.log("e: ", typeof e);
+console.log("f: ", typeof f);
+console.log("g: ", typeof g);
+console.log("h: ", typeof h);
+console.log("j: ", typeof j);
+console.log("k: ", typeof k);
+console.log("l: ", typeof l);
+console.log("m: ", typeof m);
+// Задача #3
+let goodNumber = 17;
+let anotherNumber = goodNumber;
+anotherNumber = 77;
+console.log(goodNumber); 
+console.log(anotherNumber);
+let Customer = {
+     name: 'John',
+      age: 17
+    };
+let anotherCustomer = Customer;
+anotherCustomer.age = 20;
+console.log(Customer.age);
+console.log(anotherCustomer.age);
+
+
+
+
